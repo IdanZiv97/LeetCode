a2fde864f8f3f8e31ce0f497eb375bfac11b8f84
IdanZiv97 <idanziv7@gmail.com>
2021-10-29 15:58:06 +0300

    An O(n) solution.
    Used a HashMap because I can search for an element in O(1).
    The idea is that we defined the Hashmap as <value, index in nums>
    So for each element we want to see if there is an element with the
    the value target - nums[i]. if so you can return the indexes.

--- Easy/Two Sum/twoSum.java
+++ Easy/Two Sum/twoSum.java
@@ -1,18 +1,20 @@
+import java.util.HashMap;
+
 /**
  * twoSum
  */
 public class twoSum {
     public int[] solution(int[] nums, int target) {
-        int[] indexes = new int[2];
+        //Hash map of vlaue (as key) and its index in nums
+        HashMap<Integer, Integer> indexes = new HashMap<Integer, Integer>();
+        //for each value we need to find target - value in nums,
         int size = nums.length;
-        for (int first = 0; first < size; first++) {
-            for (int second = first + 0; second < size; second ++) {
-                if (nums[first] + nums[second] == target) {
-                    indexes[0] = first;
-                    indexes[1] = second;
-                }
+        for (int i = 0; i < size; i++) {
+            if (indexes.containsKey(target - nums[i])) {
+                return new int[]{indexes.get(target - nums[i]), i};
             }
+            indexes.put(nums[i], i);
         }
-        return indexes;
+        throw new IllegalArgumentException("Found no solution!\n");
     }
 }
