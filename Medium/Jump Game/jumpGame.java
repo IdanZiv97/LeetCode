dd983d394a626f0bcefabad7ab362797093a4676
IdanZiv97 <idanziv7@gmail.com>
2021-11-02 16:27:39 +0200

    Better solution

--- Medium/Jump Game/jumpGame.java
+++ Medium/Jump Game/jumpGame.java
@@ -1,21 +1,33 @@
-import java.util.zip.CRC32C;
-
 /**
  * jumpGame
  */
 public class jumpGame {
     public boolean canJump(int[] nums) {
         int size = nums.length;
-        int currentIndex = 0;
-        int maxJump = 0;
-        //If we can reach the end of the array than the max jump from the first index will always be less than the size of it
-        for (; currentIndex < size && currentIndex <= maxJump; currentIndex++) {
-            maxJump = Math.max(currentIndex + nums[currentIndex], maxJump);
-        }
-        if (currentIndex == size) {
+        int index = 0;
+        int lastIndex = size - 1;
+        //When you have only one item it is always true
+        if (size == 1) {
             return true;
-        } else {
-            return false;
         }
+        //The min travel possible
+        int distance = nums[0];
+        for (; index < size; index++) {
+            //when we cant travel no further
+            if (distance < 0) {
+                return false;
+            }
+            //we want to see, from each index what is the maximum distance we can jump
+            //Regarding the value from the current index
+            distance = Math.max(distance, nums[index]);
+            //check from this index you can reach last index
+            if (index + distance >= lastIndex) {
+                return true;
+            }
+            //We move to the next index - decrease the distance
+            distance--;
+        }
+        //if this point is reached we are in the case where we finised our jumps on the last index
+        return true;
     }
 }
